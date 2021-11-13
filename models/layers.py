2ce8ad6f2ca6f01555a7d06bd9078635c07d5037
IbrahimL <ibrahim.laiche@g.enp.edu.dz>
2021-11-13 16:49:10 +0100

    first commit

---
+++ models/layers.py
@@ -0,0 +1,25 @@
+import tensorflow as tf 
+
+class EdgeConv(tf.keras.layers.Layer):
+    '''
+    '''
+    def __init__(self, params):
+        super(EdgeConv, self).__init__()
+
+    def build(self):
+        pass
+
+    def call(self, node_features):
+        pass
+
+class EdgeConvE(tf.keras.layers.Layer):
+    '''
+    '''
+    def __init__(self. params):
+        super(EdgeConvE, self).__init__()
+
+    def build(self):
+        pass
+
+    def call(self, node_features, edge_attriutes):
+        pass
