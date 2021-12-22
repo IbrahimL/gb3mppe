cc861cda28b77be5b4607ca7149324956347dff1
IbrahimL <ibrahim.laiche@g.enp.edu.dz>
2021-12-22 19:09:13 +0100

    EdgeConv works for batches

--- lib/models/layers.py
+++ lib/models/layers.py
@@ -1,7 +1,9 @@
+from functools import reduce
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Layer
+from tensorflow_graphics.geometry.convolution.graph_convolution import edge_convolution_template
 from tensorflow.python.ops.gen_math_ops import Add
 
 class MLP(Model):
@@ -28,50 +30,19 @@ class EdgeConv(Layer):
         super(EdgeConv, self).__init__()
         self.h_theta = h_theta
 
-    def _aggregate(self, inputs):
+    def _aggregate(self, x_v, x_vp):
         '''
         '''
-        x_v, x_vp = inputs
         return self.h_theta(tf.concat([x_v, x_vp-x_v], -1))
-
-    def _prep_features(self, Adjacency, node_features):
-        '''
-        '''
-        A_temp = tf.cast(Adjacency, tf.int32) - tf.eye(Adjacency.shape[-1], dtype=tf.int32)
-        Deg = tf.reduce_sum(A_temp, axis=0)
-        Deg = tf.cast(Deg, tf.int32)
-        Feat_rep_n = tf.repeat(node_features, Deg, axis=0)
-        Feat_rep_v = tf.gather(node_features, tf.where(A_temp==1)[:,1])
-        return Feat_rep_n, Feat_rep_v, Deg
-
-    def _convolution(self, inputs):
-        '''
-        '''
-        Adjacency, node_features = inputs
-        Feat_rep_n, Feat_rep_v, Deg = self._prep_features(Adjacency, node_features)
-        res = tf.squeeze(tf.vectorized_map(self._aggregate, (Feat_rep_n, Feat_rep_v)))
-        max_id = tf.vectorized_map(tf.norm, res)
-        res = tf.split(res, Deg)
-        max_id = tf.split(max_id, Deg)
-        get_ind = lambda x: tf.math.argmax(x, axis=-2)
-        max_id = tf.vectorized_map(get_ind, max_id)
-        res = tf.ragged.stack(res)
-        res_tensor = tf.gather(res, max_id, batch_dims=1)
-        return res_tensor.to_tensor()
     
     def call(self, Adjacency, node_features):
-        if len(Adjacency.shape) == 2:
-            inputs = (Adjacency, node_features)
-            return self._convolution(inputs)
-        
-        #return tf.vectorized_map(self._convolution, inputs)
-        res = [0]*Adjacency.shape[0]
-        for i in range(Adjacency.shape[0]):
-            # This works but might be slower
-            # idk why tf.vectorized_map didn't work
-            inputs = (Adjacency[i], node_features[i])
-            res[i] = self._convolution(inputs)
-        return tf.stack(res)
+        return edge_convolution_template(data=node_features, 
+                                         neighbors=Adjacency, 
+                                         sizes=None,
+                                         edge_function=self._aggregate,
+                                         reduction='max',
+                                         edge_function_kwargs={},
+                                         name='edge_conv')
             
 
 
@@ -101,24 +72,21 @@ class EdgeConvE(Layer):
 if __name__ == "__main__":
     # Test MLP class
     h_dim = 4
-    o_dim = 4
+    o_dim = 8
     mlp = MLP(h_dim, o_dim)
     x = tf.random.uniform((10, 80))
-    # Test EdgeConv
-    ec = EdgeConv(mlp)
-    A = tf.convert_to_tensor([[1, 0, 1, 0],[0, 1, 1, 1],[1, 1, 1, 0],[0, 1, 0, 1]])
-    Feat = tf.convert_to_tensor([[1, 2],[2, 3], [4, 5], [5, 6]])
-    print("test 1:")
-    print(ec(A, Feat))
     # Test EdgeConv (Batch)
     ec = EdgeConv(mlp)
     A = tf.convert_to_tensor([[[1, 0, 1, 0],[0, 1, 1, 1],[1, 1, 1, 0],[0, 1, 0, 1]], 
-                              [[1, 0, 1, 0],[0, 1, 1, 1],[1, 1, 1, 0],[0, 1, 0, 1]]])
-    Feat = tf.convert_to_tensor([[[1, 2],[2, 3], [4, 5], [5, 6]],
-                                 [[1, 2],[2, 3], [4, 5], [5, 6]]])
+                              [[1, 0, 1, 0],[0, 1, 1, 1],[1, 1, 1, 0],[0, 1, 0, 1]]], dtype=tf.float32)
+    A = tf.sparse.from_dense(A)
+    Feat = tf.convert_to_tensor([[[1, 2, 6],[2, 3, 2], [4, 5, 10], [5, 6, -1]],
+                                 [[1, 2, -3],[2, 3, 0.5], [4, 5, -9], [5, 6, 0]]], dtype=tf.float32)
     '''
     A = tf.convert_to_tensor([[1, 0, 1, 0],[0, 1, 1, 1],[1, 1, 1, 0],[0, 1, 0, 1]])
     Feat = tf.convert_to_tensor([[1, 2],[2, 3], [4, 5], [5, 6]])
     '''
     print("test 2")
-    print(ec(A,Feat))
+    print(A.shape)
+    print(Feat.shape)
+    print(ec(A,Feat).shape)
