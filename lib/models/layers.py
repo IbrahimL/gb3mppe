5a666e79d59a7e256ea351d3df5104fc6d98dc33
IbrahimL <ibrahim.laiche@g.enp.edu.dz>
2021-12-22 23:57:52 +0100

    WIP

--- lib/models/layers.py
+++ lib/models/layers.py
@@ -1,9 +1,11 @@
 from functools import reduce
+from typing import Any, Callable, Dict
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Layer
 from tensorflow_graphics.geometry.convolution.graph_convolution import edge_convolution_template
+from tensorflow_graphics.geometry.convolution import utils
 from tensorflow.python.ops.gen_math_ops import Add
 
 class MLP(Model):
@@ -22,6 +24,61 @@ class MLP(Model):
         x = self.linear_hid2(x)
         x = self.linear_out(x)
         return x
+ 
+#https://github.com/tensorflow/graphics/blob/master/tensorflow_graphics/geometry/convolution/graph_convolution.py   
+def edge_convolution_E_template(data, neighbors, edges_features, sizes, edge_function, reduction, edge_function_kwargs, name="Layer_E"):
+  with tf.name_scope(name):
+    data = tf.convert_to_tensor(value=data)
+    edges_features = tf.convert_to_tensor(value=edges_features)
+    neighbors = tf.compat.v1.convert_to_tensor_or_sparse_tensor(value=neighbors)
+    if sizes is not None:
+      sizes = tf.convert_to_tensor(value=sizes)
+
+    data_ndims = data.shape.ndims
+    utils.check_valid_graph_convolution_input(data, neighbors, sizes)
+
+    # Flatten the batch dimensions and remove any vertex padding.
+    if data_ndims > 2:
+      if sizes is not None:
+        sizes_square = tf.stack((sizes, sizes), axis=-1)
+      else:
+        sizes_square = None
+      x_flat, unflatten = utils.flatten_batch_to_2d(data, sizes)
+      adjacency = utils.convert_to_block_diag_2d(neighbors, sizes_square)
+    else:
+      x_flat = data
+      adjacency = neighbors
+
+    adjacency_ind_0 = adjacency.indices[:, 0]
+    adjacency_ind_1 = adjacency.indices[:, 1]
+    vertex_features = tf.gather(x_flat, adjacency_ind_0)
+    neighbor_features = tf.gather(x_flat, adjacency_ind_1)
+    edge_features = tf.gather(edges_features, [adjacency_ind_0, adjacency_ind_1])
+    output = edge_function(vertex_features, neighbor_features, edge_features,
+                                  **edge_function_kwargs)
+
+    if reduction == "weighted":
+      output_weighted = output * tf.expand_dims(
+          adjacency.values, -1)
+      features = tf.math.unsorted_segment_sum(
+          data=output_weighted,
+          segment_ids=adjacency_ind_0,
+          num_segments=tf.shape(input=x_flat)[0])
+    elif reduction == "max":
+      features = tf.math.segment_max(
+          data=output, segment_ids=adjacency_ind_0)
+    else:
+      raise ValueError("The reduction method must be 'weighted' or 'max'")
+
+    features.set_shape(
+        features.shape.merge_with(
+            (tf.compat.dimension_value(x_flat.shape[0]),
+             tf.compat.dimension_value(output.shape[-1]))))
+
+    if data_ndims > 2:
+      features = unflatten(features)
+    return features
+
 
 class EdgeConv(Layer):
     def __init__(self, h_theta):
@@ -49,27 +106,27 @@ class EdgeConv(Layer):
 class EdgeConvE(Layer):
     '''
     '''
-    def __init__(self, params):
+    def __init__(self, h_theta):
         super(EdgeConvE, self).__init__()
+        self.h_theta = h_theta
         
-    def _aggregate(self, inputs):
+    def _aggregate(self, x_v, x_vp, e_v_vp):
         '''
         '''
-        x_v, x_vp = inputs
-        return self.h_theta(tf.concat([x_v, x_vp-x_v,edge_attributes], -1))
-    
-    
-    def build(self):
-        pass
-
+        return self.h_theta(tf.concat([x_v, x_vp-x_v, e_v_vp], -1))
     
     def call(self, Adjacency, node_features, edge_attributes):
-        inputs = (Adjacency, node_features, edge_attributes)
-        if len(Adjacency.shape) == 2:
-            return self._convolution(inputs)
-        return tf.vectorized_map(self._convolution, inputs)
+        return edge_convolution_E_template(data=node_features, 
+                                         neighbors=Adjacency,
+                                         edges_features=edge_attributes, 
+                                         sizes=None,
+                                         edge_function=self._aggregate,
+                                         reduction='max',
+                                         edge_function_kwargs={},
+                                         name='edge_conv')
 
 if __name__ == "__main__":
+    import numpy as np
     # Test MLP class
     h_dim = 4
     o_dim = 8
@@ -80,13 +137,20 @@ if __name__ == "__main__":
     A = tf.convert_to_tensor([[[1, 0, 1, 0],[0, 1, 1, 1],[1, 1, 1, 0],[0, 1, 0, 1]], 
                               [[1, 0, 1, 0],[0, 1, 1, 1],[1, 1, 1, 0],[0, 1, 0, 1]]], dtype=tf.float32)
     A = tf.sparse.from_dense(A)
+    edge_att = tf.random.uniform([2,4,4,3])
     Feat = tf.convert_to_tensor([[[1, 2, 6],[2, 3, 2], [4, 5, 10], [5, 6, -1]],
                                  [[1, 2, -3],[2, 3, 0.5], [4, 5, -9], [5, 6, 0]]], dtype=tf.float32)
-    '''
-    A = tf.convert_to_tensor([[1, 0, 1, 0],[0, 1, 1, 1],[1, 1, 1, 0],[0, 1, 0, 1]])
-    Feat = tf.convert_to_tensor([[1, 2],[2, 3], [4, 5], [5, 6]])
-    '''
-    print("test 2")
-    print(A.shape)
-    print(Feat.shape)
-    print(ec(A,Feat).shape)
+    
+    print("test Edge Convolution")
+    print('adjacency:', A.shape)
+    print('node features:', Feat.shape)
+    print('output:', ec(A,Feat).shape)
+    ### TEST EDGE CONV E
+    ece = EdgeConvE(mlp)
+    print("test Edge Convolution-E")
+    print('adjacency:', A.shape)
+    print('node features:', Feat.shape)
+    print('edge attributes:', edge_att.shape)
+    print('output:', ece(A,Feat,edge_att).shape)
+    
+    
