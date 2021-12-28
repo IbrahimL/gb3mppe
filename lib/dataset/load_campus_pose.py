0030656fe806e3f158d3f22b1a3a84cde3e30518
karim416 <rimks.97@gmail.com>
2021-12-28 01:39:45 +0100

    petite erreur dans le precedent depot

--- lib/dataset/load_campus_pose.py
+++ lib/dataset/load_campus_pose.py
@@ -9,13 +9,13 @@ class voxel_campus():
         a_file.close()
 # l'indice 0 correspond  à l image 305
     def get_data_campus(self,frame=350,camera=0,joints='joints_3d'):
-        if frame in list(range(350, 471)) + list(range(650, 751)) :
-            frame-=350
+        if frame in list(range(704,2000)) :
+            frame-=704
             return self.output [frame*3+camera][joints]
         else :
             print('Frame not in list')
             return []
         
 if __name__ == "__main__":
-    print("joints 2d de l'image 400 , camera 0: ")
-    print(voxel_campus().get_data_campus(400,0,'joints_2d'))
+    print("joints 2d de l'image 704 , camera 0: ")
+    print(voxel_campus().get_data_campus(704,0,'joints_2d'))
